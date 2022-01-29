199d083f1dba6528fcfc9d4061526288c5597767
Daniel Marques <danielvictorsm@gmail.com>
2022-01-29 13:42:53 -0300

    feat(Tool): Alteração de rotas

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
 import api from '../../services/api';
 import { ITool } from '../../models/ITool'
+import { IToolRequest } from '../../models/IToolRequest';
 import './index.css';
-
 
 
 export default function Index({history}) {
@@ -12,14 +12,35 @@ export default function Index({history}) {
     const [tagFilter, setTagFilter] = useState(false);
     const [titleFilter, setTitleFilter] = useState(false);
     const [show, setShow] = useState(false);
+    const [addShow, setAddShow] = useState(false);
     const [id, setId] = useState('');
+    const [toolRequest, setToolRequest] = useState<IToolRequest>();
+    const [title, setTitle] = useState('');
+    const [link, setLink] = useState('');
+    const [description, setDescription] = useState('');
+    const [tags, setTags] = useState<string[]>([]);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleDeleteModal = () => setShow(!show);
+    const handleAddModal = () => setAddShow(!addShow);
 
     useEffect(() => {
         loadTools()
     }, []);
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault();
+        setToolRequest({
+            title,
+            link,
+            description,
+            tags
+        });
+        console.log(toolRequest)
+        const response = await api.post("/v1/tools", toolRequest)
+        console.log(response)
+        handleAddModal()
+        history.push("/v1/tools")
+    }
 
     async function loadTools() {
         const response = await api.get("/v1/tools");
@@ -39,10 +60,6 @@ export default function Index({history}) {
         } 
     }
 
-    async function addTool() {
-        history.push("/v1/tools/form");
-    }
-
     async function removeTool(id: string) {
         const response = await api.delete(`v1/tools/${id}`);  
         console.log(response) 
@@ -57,15 +74,43 @@ export default function Index({history}) {
                     <Form.Check type="checkbox" label="By tags only" onChange={e => setTagFilter(true)}/>
                     <Form.Check type="checkbox" label="By title only" onChange={e => setTitleFilter(true)}/>
                 </Form.Group>
-                <Button variant="primary" type="submit" onClick={addTool}>
+                <Button variant="primary" type="submit" onClick={handleAddModal}>
                     Add Tool
                 </Button>
             </div>
+            <Modal show={addShow} onHide={handleAddModal}>
+            <Form onSubmit={onSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicTitle">
+                    <Form.Label>Tool Name</Form.Label>
+                    <Form.Control type="text" placeholder="Tool Name" 
+                    onChange={e => setTitle(e.target.value)}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicLink">
+                    <Form.Label>Tool Link</Form.Label>
+                    <Form.Control type="text" placeholder="Tool Link" 
+                    onChange={e => setLink(e.target.value)}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicDescription">
+                    <Form.Label>Tool Description</Form.Label>
+                    <Form.Control type="text" placeholder="Tool Description" 
+                    onChange={e => setDescription(e.target.value)}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicTags">
+                    <Form.Label>Tags</Form.Label>
+                    <Form.Control type="text" placeholder="Tags" 
+                    onChange={e => setTags(e.target.value.split(","))}
+                    />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Add Tool
+                </Button>
+            </Form>
+            </Modal>
             <ul>
             {tools.map(tool => (
                 <li id='tool-li' key={tool.id}>
                     <a href='{tool.link}'>{tool.title}</a>
-                    <button onClick={e => {setId(tool.id);handleShow()}}>remove</button>
+                    <button onClick={e => {setId(tool.id);handleDeleteModal()}}>remove</button>
                     <p>{tool.description}</p>
                     <footer><b>
                     {tool.tags.map(tag => (
@@ -75,16 +120,16 @@ export default function Index({history}) {
                 </li>
             ))}
             </ul>
-            <Modal show={show} onHide={handleClose}>
+            <Modal show={show} onHide={handleDeleteModal}>
                 <Modal.Header closeButton>
                 <Modal.Title>Remove Tool</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>Are you sure you want to remove this Tool?</Modal.Body>
                 <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
+                <Button variant="secondary" onClick={handleDeleteModal}>
                     Cancel
                 </Button>
-                <Button variant="primary" onClick={e => {removeTool(id);handleClose()}}>
+                <Button variant="primary" onClick={e => {removeTool(id);handleDeleteModal()}}>
                     Yes, remove
                 </Button>
                 </Modal.Footer>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import ToolForm from './pages/Form/index';
 import Index from './pages/Index';
 
 export default function Routes() {
     return (
         <Switch>
-            <Route path="/v1/tools" exact component={Index} />
-            <Route path="/v1/tools/form" exact component={ToolForm} />
+            <Route path="/" component={Index} />
         </Switch>
     );
 }
